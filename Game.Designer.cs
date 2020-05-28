20f2c60dd9c032f7e2b60c36af4e59db4b297391
Reinis P <reinisp223@gmail.com>
2020-05-28 16:33:04 +0300

    version 1

---
+++ Game.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace VirusEscape
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.virusStartScreen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.virusStartScreen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("MV Boli", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(34, 163);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(182, 39);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Virus Escape";
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPlay.Location = new System.Drawing.Point(41, 214);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(164, 49);
+            this.buttonPlay.TabIndex = 1;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            // 
+            // virusStartScreen
+            // 
+            this.virusStartScreen.Location = new System.Drawing.Point(41, 12);
+            this.virusStartScreen.Name = "virusStartScreen";
+            this.virusStartScreen.Size = new System.Drawing.Size(164, 148);
+            this.virusStartScreen.TabIndex = 2;
+            this.virusStartScreen.TabStop = false;
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 293);
+            this.Controls.Add(this.virusStartScreen);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.labelTitle);
+            this.Name = "Game";
+            this.Text = "Virus Escape";
+            ((System.ComponentModel.ISupportInitialize)(this.virusStartScreen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.PictureBox virusStartScreen;
+    }
+}
+
---
+++ Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VirusEscape
+{
+    public partial class Game : Form
+    {
+        public Game()
+        {
+            InitializeComponent();
+            InitializeForm();
+        }
+        private void InitializeForm()
+        {
+            this.MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.Height = 332;
+            this.Width = 259;
+            this.BackColor = Color.Green;
+            StartScreen();
+        }
+        private void StartScreen()
+        {
+            labelTitle.Location = new Point(34, 163);
+            buttonPlay.Location = new Point(41, 214);
+            virusStartScreen.Location = new Point(41, 12);
+            virusStartScreen.BackgroundImage = Properties.Resources.virusStartScreen;
+            virusStartScreen.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+    }
+}
